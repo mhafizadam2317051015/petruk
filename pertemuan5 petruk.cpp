1ed0047caabca9959514069f0c01c060beb708a9
adddam <151291849+mhafizadam2317051015@users.noreply.github.com>
2024-03-20 10:26:23 +0700

    pertemuan5 petruk.cpp

--- pertemuan5 petruk.cpp
+++ pertemuan5 petruk.cpp
@@ -13,3 +13,136 @@ int main(){
 	cin >> Mhs.npm;
 	
 	cout << Mhs.nama << " " << Mhs.npm;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream> 
+using namespace std;
+
+struct Mahasiswa{
+	public:
+		string nama, npm;
+		
+		void Perkenalan(){
+			cout << "Nama: " << nama << endl;
+			cout << "NPM: " << nama << endl;
+		}
+};
+
+int main(){
+	Mahasiswa Mhs;
+	
+	cin >> Mhs.nama;
+	cin >> Mhs.npm;
+	
+	mhs.perkenalan();
+}\
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream> 
+using namespace std;
+
+struct Mahasiswa{
+	public:
+		string nama, npm;
+		
+		Mahasiswa(){
+			this->nama = nama;
+			this->npm = b;
+		}
+};
+
+int main(){
+	Mahasiswa Mhs("Reza","2117");
+	
+	cout << "Nama: " << Mhs.nama << endl;
+	cout << "Npm: " << Mhs.npm << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream> 
+using namespace std;
+
+struct Mahasiswa{
+	private:
+		string nama;
+	
+	public:
+		void setNama(string nama) {
+			this->nama = nama;
+		}
+		
+		string getNama() {
+			return nama;
+		}
+};
+
+int main(){
+	Mahasiswa Mhs;
+	
+	mhs.setNama("Anta");
+	
+	cout << "Nama: " << Mhs.getNama() << endl;
+}
