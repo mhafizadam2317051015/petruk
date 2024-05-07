e2d4c0cae8283cba30ecad2058c05128b6a1d408
adddam <151291849+mhafizadam2317051015@users.noreply.github.com>
2024-05-07 16:29:34 +0700

    soal4.cpp

--- soal4.cpp
+++ soal4.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main(){
+  int N;
+  string kata;
+  unsigned int angka;
+  cin >> N;
+  stack < int >tes;
+  for (int i = 0; i < N; i++)
+	{
+	  cin >> kata;
+	  if (kata == "push")
+		{
+
+		  cin >> angka;
+		  tes.push (angka);
+		}
+	  else if (kata == "pop")
+		{
+		  if (!tes.empty ())
+			{
+			  tes.pop ();
+			}
+		}
+	  stack < int >hasil = tes;
+	  while (!hasil.empty ())
+		{
+		  cout << hasil.top () << " ";
+		  hasil.pop ();
+		}
+	  cout << endl;
+	}
+  return 0;
+}
 
