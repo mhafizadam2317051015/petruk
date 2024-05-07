a184f1001db0b4103e6e85ee81a1b6cc32545897
adddam <151291849+mhafizadam2317051015@users.noreply.github.com>
2024-05-07 16:07:30 +0700

    soal3.cpp

--- soal3.cpp
+++ soal3.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+
+    int numbers[N];
+    for (int i = 0; i < N; ++i) {
+        cin >> numbers[i];
+    }
+
+    for (int i = 1; i < N; i += 2) {
+        cout << numbers[i] << " ";
+    }
+
+    return 0;
+}
 
